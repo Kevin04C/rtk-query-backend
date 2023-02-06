3bc6daa54ae799d925ddfbeb477b693d82e424c6
Kevin04C <kevincespedes2004@gmail.com>
2023-02-05 19:32:52 -0500

    Add validateJWT

---
+++ middlewares/validateJWT.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken'
+import { request, response } from 'express'
+
+export const validateJWT = (req = request, res = response, next) => {
+  const { authorization } = req.headers
+
+  if (!authorization) {
+    return res.status(400).json({
+      ok: false,
+      msg: 'there not token in request'
+    })
+  }
+
+  const [bearer, token] = authorization.split(' ')
+  if (bearer !== 'Bearer') {
+    return res.status(400).json({
+      ok: false,
+      msg: 'header authorization invalid'
+    })
+  }
+
+  try {
+    const { name, id } = jwt.verify(token, process.env.SECRECT_TOKEN_JWT)
+    req.user = { id, name }
+  } catch (error) {
+    res.status(500).json({
+      ok: false,
+      msg: 'Internal server error'
+    })
+  }
+
+  next()
+}
